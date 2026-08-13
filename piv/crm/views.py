86e8648e765cc2b6781cef141495578702067b5c
george <rszgeorge@gmail.com>
2026-08-13 14:26:35 +0300

    login and requirements added

--- piv/crm/views.py
+++ piv/crm/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def index(request):
-    return HttpResponse("You're at the crm index")
+    if request.user.is_authenticated:
+        return HttpResponse(f"Hello, {request.user.username}. You're at the crm index")
+    return HttpResponse("You're at the crm index. <a href='/login/'>Login</a>")
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            messages.error(request, "Invalid credentials")
+    return render(request, "crm/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("index")
